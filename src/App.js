d7a5394c0884639207682491822bb892a55aca18
Riley Cheung <tcheung25@mylangara.ca>
2024-06-22 09:50:42 -0700

    Submit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>CPSC2650 React (TingHin Cheung)</p>
+        <ul>
+          <li>Week 1: HTML CSS</li>
+          <li>Week 2: Git</li>
+          <li>Week 3: JavaScript</li>
+          <li>Week 4: Node/ Express/ Mongo</li>
+          <li>Week 5: Redis/ Postman</li>
+          <li>Week 6: Mid term/ A11y/ Websocket</li>
+          <li>Week 7: React</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
